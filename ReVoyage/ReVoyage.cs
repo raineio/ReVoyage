4fa1c3d02df8a5bfdca2858bba94d13ac59145e4
Raine <Raine@DESKTOP-L18IS40>
2023-01-31 11:09:32 +0100

    Add project files.

---
+++ ReVoyage/ReVoyage.cs
@@ -0,0 +1,22 @@
+﻿using Dalamud;
+using Dalamud.Plugin;
+
+namespace ReVoyage
+{
+    public class ReVoyage : IDalamudPlugin
+    {
+        public string Name => "ReVoyage";
+
+        private readonly DalamudPluginInterface _dalamudPluginInterface;
+
+        public ReVoyage(DalamudPluginInterface pluginInterface)
+        {
+            _dalamudPluginInterface = pluginInterface;
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
